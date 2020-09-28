06b40098bedc53bfcb9121dbfa779357656bc77e
CR <christopher_ruiz1@baylor.edu>
2020-09-27 21:04:15 -0500

    made some changes

---
+++ Death_valley_3.py
@@ -0,0 +1,60 @@
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+def get_weather_data(filename, dates, highs, lows, date_index, high_index,
+        low_index):
+    """Get the highs and lows from a data file."""
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+
+        # Get dates, and high and low temperatures from this file.
+        for row in reader:
+            current_date = datetime.strptime(row[date_index], '%Y-%m-%d')
+            try:
+                high = int(row[high_index])
+                low = int(row[low_index])
+            except ValueError:
+                print(f"Missing data for {current_date}")
+            else:
+                dates.append(current_date)
+                highs.append(high)
+                lows.append(low)
+
+# Get weather data for Sitka.
+filename = 'sitka_weather_2018_simple.csv'
+dates, highs, lows = [], [], []
+get_weather_data(filename, dates, highs, lows, date_index=2, high_index=5,
+        low_index=6)
+
+# Plot Sitka weather data.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.6)
+ax.plot(dates, lows, c='blue', alpha=0.6)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.15)
+
+# Get weather data for Death Valley.
+filename = 'death_valley_2018_simple.csv'
+dates, highs, lows = [], [], []
+get_weather_data(filename, dates, highs, lows, date_index=2, high_index=4,
+        low_index=5)
+
+# Add Death Valley data to current plot.
+ax.plot(dates, highs, c='red', alpha=0.3)
+ax.plot(dates, lows, c='blue', alpha=0.3)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.05)
+
+# Format plot.
+title = "Temperature Comparison Between SITKA AIRPORT, AK US and DEATH VALLEY,CA US"
+
+plt.title(title, fontsize=16)
+plt.xlabel('', fontsize=12)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=12)
+plt.tick_params(axis='both', which='major', labelsize=12)
+plt.ylim(10, 130)
+
+plt.show()
